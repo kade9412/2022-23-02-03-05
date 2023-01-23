40cb6a4fd43efb8d9e3bb1abc58904cbda5fad4a
Nicholas DeBord <123391207+kade9412@users.noreply.github.com>
2023-01-23 15:40:27 +0000

    completed the 2nd version

--- .ccls-cache/@home@runner@2022-23-02-03-05/main.c
+++ .ccls-cache/@home@runner@2022-23-02-03-05/main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
-#include <string.h>
+#include<string.h>
+
+
 int main() {
-//TODO write a program that reverses a character array
-char s1[] = "Hello, World";
-size_t l1 = strlen(s1);
-  while (l1 != -1) { // while the length of the remaining prints the last character in the array
-    printf("%c",s1[l1]);
-    l1--;
+
+  char s1[] = "hello,World!";
+  int len = strlen(s1) -1;
+
+  while (len >= 0){
+    printf("%c\n", s1[len]);
+    len--;
   }
+  return 0;
 }
